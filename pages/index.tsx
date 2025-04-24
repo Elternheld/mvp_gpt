1e28fbd322a08f8d48cdca75abd5609820deed2d
Elternheld <elternheld@gmail.com>
2025-04-24 23:00:49 +0200

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import ActivityFetcher from "@/components/ActivityFetcher"
+import ActivityFetcher from "../components/ActivityFetcher"
 
 export default function Home() {
   return (
