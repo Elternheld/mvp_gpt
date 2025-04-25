01464ec6dabe21e4d222bf5a8b0cf239547d45da
Elternheld <elternheld@gmail.com>
2025-04-25 16:27:38 +0200

    1

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,6 +1,6 @@
-
 "use client"
 
+import { useToast } from "@/hooks/use-toast"
 import {
   Toast,
   ToastClose,
@@ -9,7 +9,6 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
-import { useToast } from "@/hooks/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
@@ -21,9 +20,7 @@ export function Toaster() {
           <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
+              {description && <ToastDescription>{description}</ToastDescription>}
             </div>
             <ToastClose />
           </Toast>
