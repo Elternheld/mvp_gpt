9d0f20fbe1ed4fb6e0a94890784a8cb9d4f0bb28
Elternheld <elternheld@gmail.com>
2025-04-25 05:51:55 +0200

    1

---
+++ .next/static/chunks/825.7881edb4ddb7b99d.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[825],{9825:function(e,t,s){s.d(t,{clerkDevelopmentCache:function(){return n},createKeylessModeMessage:function(){return createKeylessModeMessage}});var a=s(2055);let createKeylessModeMessage=e=>`
+\x1b[35m
+[Clerk]:\x1b[0m You are running in keyless mode.
+You can \x1b[35mclaim your keys\x1b[0m by visiting ${e.claimUrl}
+`,n=function(){if((0,a.vf)())return s.g.__clerk_internal_keyless_logger||(s.g.__clerk_internal_keyless_logger={__cache:new Map,log:function({cacheKey:e,msg:t}){var s;this.__cache.has(e)&&Date.now()<((null==(s=this.__cache.get(e))?void 0:s.expiresAt)||0)||(console.log(t),this.__cache.set(e,{expiresAt:Date.now()+6e5}))},run:async function(e,{cacheKey:t,onSuccessStale:s=6e5,onErrorStale:a=6e5}){var n,c;if(this.__cache.has(t)&&Date.now()<((null==(n=this.__cache.get(t))?void 0:n.expiresAt)||0))return null==(c=this.__cache.get(t))?void 0:c.data;try{let a=await e();return this.__cache.set(t,{expiresAt:Date.now()+s,data:a}),a}catch(e){throw this.__cache.set(t,{expiresAt:Date.now()+a}),e}}}),globalThis.__clerk_internal_keyless_logger}()}}]);
